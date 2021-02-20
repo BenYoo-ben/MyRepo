836673d6f2b996189c481242deb84613c3cc368e
BenYoo-yoo <neverknown0909@gmail.com>
2021-02-20 22:41:17 +0900

    makefile, arguement fix, time measure fix, pcap read/parse added

--- DDos/src/ddos/body_buffering.h
+++ DDos/src/ddos/body_buffering.h
@@ -4,6 +4,40 @@
 
 #define __BODY_BUFFERING_REQUEST_MSG_SIZE__ 100
 
-void body_buffering_print_usage(int mode);
-void body_buffering_run(char *argv[], int mode);
-#endif
+/**
+	@fn void body_buffering_print_usage(void);
+	@brief function: print body_buffering usage
+	@date 2021/02/20
+	@param void
+	@return void
+*/
+void body_buffering_print_usage(void);
+
+/**
+	@fn void *generate_body_buffering(void *data);
+	@brief function: body_buffering generate thread
+	@date 2021/02/20
+	@param void *data NULL
+	@return void * NULL
+*/
+void* generate_body_buffering(void *data);
+
+/**
+	@fn void *body_buffering_time_check(void *data);
+	@brief function: body_buffering time check thread
+	@date 2021/02/20
+	@param void *data NULL
+	@return void * NULL
+*/
+void* body_buffering_time_check(void *data);
+
+/**
+	@fn void *body(void *data);
+	@brief function: body_buffering main function
+	@date 2021/02/20
+	@param char *argv[]: arguments
+	@return void
+*/
+void body_buffering_main(char *argv[]);
+
+#endif // ifndef BODY_BUFFERING
--- DDos/src/ddos/udp_flood.h
+++ DDos/src/ddos/udp_flood.h
@@ -8,52 +8,51 @@
 	@struct udphdr
 	@brief udp header
 	@date 2021/02/13
-	@author 김한수(cpm0722@gmail.com)
 */
-struct udphdr {
+struct udphdr
+{
 	u_short src_port;   /// source port (0)
 	u_short dest_port;  /// destination port
 	u_short len;        /// data length
 	u_short checksum;   /// checksum (0)
 	char data[1024];    /// data (maximum length: 1024)
 };
-#endif
+#endif // ifndef UDP_HDR
 
 /**
 	@fn void udp_flood_print_usage(void);
-	@brief function: print udp usage
+	@brief function: print udp_flooding usage
 	@date 2021/02/13
-	@author 김한수(cpm0722@gmail.com)
 	@param void
 	@return void
 */
 void udp_flood_print_usage(void);
+
 /**
 	@fn void *generate_udp_request(void *);
-	@brief function: udp request thread
+	@brief function: udp_flooding generate thread
 	@date 2021/02/13
-	@author 김한수(cpm0722@gmail.com)
-	@param void * NULL
-	@return void * data NULL
+	@param void *data NULL
+	@return void * NULL
 */
-void *generate_udp_request(void *);
+void *generate_udp_flood(void *data);
+
 /**
 	@fn void *udp_time_check(void *);
-	@brief function: udp time check thread
+	@brief function: udp_flooding time check thread
 	@date 2021/02/13
-	@author 김한수(cpm0722@gmail.com)
-	@param void * NULL
-	@return void * data NULL
+	@param void *data NULL
+	@return void * NULL
 */
-void *udp_time_check(void *);
+void *udp_flood_time_check(void *data);
+
 /**
 	@fn void *udp_flood_main(char *argv[]);
-	@brief function: udp main function
+	@brief function: udp_flooding main function
 	@date 2021/02/13
-	@author 김한수(cpm0722@gmail.com)
-	@param void
-	@return argv char *[]: arguments
+	@param argv char *argv[]: arguments
+	@return void
 */
 void udp_flood_main(char *argv[]);
 
-#endif
+#endif // ifndef UDP_FLOOD
