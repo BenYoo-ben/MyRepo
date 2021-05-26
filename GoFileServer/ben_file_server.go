fe66b076c40187939e590318b8fe238006a3983f
BenYoo-ben <neverknown0909@gmail.com>
2021-05-27 03:11:22 +0900

    upload/download finish for go server

--- GoFileServer/ben_file_server.go
+++ GoFileServer/ben_file_server.go
@@ -19,7 +19,7 @@ func main(){
 
     tcp_port := getPort()
 	fmt.Println("tcpport:",tcp_port)
-    listener, err := net.Listen(CONN_TYPE, CONN_HOST+":"+strconv.FormatUint(uint64(tcp_port),10))
+    listener, err := net.Listen(CONN_TYPE, ":"+strconv.FormatUint(uint64(tcp_port),10))
     if( err!= nil){
         fmt.Println("Error listening:",err.Error())
         os.Exit(1)
@@ -30,7 +30,8 @@ func main(){
     fmt.Println("This Server listening on :",strconv.FormatUint(uint64(tcp_port),10))
 
    for{
-        new_conn, err := listener.Accept()
+		fmt.Println("waitng for client...")
+		new_conn, err := listener.Accept()
         if err != nil {
              fmt.Println("Error Accepting:",err.Error())
             os.Exit(1)
@@ -70,25 +71,54 @@ func getPort() uint16{
 //go docs : fmt, bufio, net, os
 func ClientHandler(con net.Conn) {
 //   var str string
-   recvBuf := make([]byte,1024)
-   con.Read(recvBuf[:])
-	if(string(recvBuf)=="1"){
+	recvBuf := make([]byte,1024)
+  n, _ := con.Read(recvBuf[:])
+   choice_made := string(recvBuf[:n]) 
+   fmt.Println("Choise : ",choice_made)
+	if choice_made == "1" {
 	//request for download
-		con.Read(recvBuf[:])
-		file, err := os.Open("storage/"+string(recvBuf))
+		fmt.Println("Download Request recv")
+		nn, _ := con.Read(recvBuf[:])
+
+		file_name := string(recvBuf[:nn])
+		file, err := os.Open("storage/"+file_name)
 			if err != nil {
-				fmt.Println("Error read file:",string(recvBuf))
-				con.Write([]byte("ERR"))
+				fmt.Println("Error read file:",file_name)
+				con.Close()
 				return
 			}else{
 				defer file.Close()
+				fmt.Println("Download Start for :",file_name)
 				n, err  := io.Copy(con,file)
 				if( err != nil){
 				fmt.Println("Error send file")
 				return
 				}
 			fmt.Println("Successfully sent :",n)
+			con.Close()
 			}
 		return
+	}else if choice_made == "2" {
+	fmt.Println("Upload Request recv")
+	nn, _ := con.Read(recvBuf[:])
+	file_name := string(recvBuf[:nn])
+	fmt.Println("FILENAME : ",file_name)
+	file, err := os.Create("storage/"+file_name)
+		if( err!= nil ){
+			fmt.Println("Error creating file ", file_name)
+			con.Close()
+
+		}else{
+			defer file.Close()
+			fmt.Println("Upload Start for :",file_name)
+			_, err := io.Copy(file,con)
+			if(err != nil){
+				fmt.Println("Error recv file")
+				return
+			}
+
+			fmt.Println("Successfully uploaded for : ",file_name)
+			con.Close()
+		}
 	}
 }
