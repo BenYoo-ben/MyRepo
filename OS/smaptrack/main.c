78b15bf8437dae811f5be960178b26ff47a10f9b
BenYoo-ben <neverknown0909@gmail.com>
2021-08-18 16:52:05 +0900

    smaptrack proto done

--- OS/smaptrack/main.c
+++ OS/smaptrack/main.c
@@ -6,102 +6,128 @@
 
 #define __SMAPTRACK_DIR_BUFFER_SIZE__ 1024
 #define __SMAPTRACK_DATA_COUNT__ 256
+//#define __SMAPTRACK_DEBUG__ 
 
 struct smaptrack_block {
 	char lines[16][512];
 };
 
-int Smaptrack_ReadLine_CopyToBuffer(int fd, char *buffer) {
-	char local_buffer[1024];
-	char c = '\r';
-	int i = 0, k = 0;
-	while ((k = read(fd, &c, 1) > 0)) {
-		printf("%c\n",c);
-		if (c == '\r' || c == '\n') {
-			break;
-		} else {
-			local_buffer[i++] = c;
-		}
-	}
-
-	if (!k)
-		return 0;
-
-	strcpy(buffer, local_buffer);
-
-	printf("Buffer: %s : return 1\n",buffer);
-	return 1;
-}
-
+	
 int main(int argc, char *argv[]) {
 
-	if (argc != 3) {
-		printf("Usage : %s [# pid] [# duration(sec)]\n", argv[0]);
+	if (argc != 4) {
+    
+		printf("Usage : %s [# pid] [# duration(sec)] [# smap line count]\n", argv[0]);
 		exit(0);
 	}
 
+    int smap_elem_line_count = atoi(argv[3]);
 	int sleep_duration = atoi(argv[2]);
-
+    
+    long trial_count = 1;
 	char dirloc[__SMAPTRACK_DIR_BUFFER_SIZE__ ];
 
 	struct smaptrack_block smapsb[__SMAPTRACK_DATA_COUNT__ ];
+    struct smaptrack_block smapsbprev[__SMAPTRACK_DATA_COUNT__];
 
 	memset(dirloc, 0x0, __SMAPTRACK_DIR_BUFFER_SIZE__);
 
 	sprintf(dirloc, "/proc/%d/smaps", atoi(argv[1]));
 
+    printf("Start logging for %s -- line C:%d -- sleep duration --%d\n\n",dirloc,smap_elem_line_count,sleep_duration);
+    
 	int fd = open(dirloc, O_RDONLY);
 
+    int chunk_size = 16 * 512 * __SMAPTRACK_DATA_COUNT__;
+    
+    char string_buffer[chunk_size];
+    
 	while (1) {
+    
+        printf("[%ld] trial... \n\n",trial_count++);
 
 		int count = 0;
-		int i=0;
+		int i=0,j=0;
 		int fin_flag = 0;
-		while (1) {
+        lseek(fd,0,SEEK_SET);
+		memset(string_buffer,0x0,chunk_size);
+    
+        //read
+        int read_size=0;
+        int curr_read = -1;
+        while( (curr_read = read(fd,string_buffer+read_size,chunk_size))>0)
+        {
+            read_size += curr_read;
+        }
 
-			switch (i) {
-			case 0:
-			case 1:
-			case 2:
-			case 3:
-			case 4:
-			case 5:
-			case 6:
-			case 7:
-			case 8:
-			case 9:
-			case 10:
-			case 11:
-			case 12:
-			case 13:
-			case 14:
-			case 15:
-			default:
-				if(Smaptrack_ReadLine_CopyToBuffer(fd, smapsb[count].lines[i++])==0){
-					fin_flag ++;
-				}
-			}
 
-			if(fin_flag){
-				break;
-			}
-			if (i == __SMAPTRACK_DATA_COUNT__){
-				count++;
-				i=0;
-			}
-		}
-		//comparison here...
-		int j=0;
+      //string parse
+       char buffer[256];
+       int b_i=0;
+        while(i<read_size){
+        
+        if(string_buffer[i]=='\n'){
+        
+            if(j==smap_elem_line_count){
+                j=0;
+                count++;
+            }
+
+            buffer[b_i] = '\0';
+            strcpy(smapsb[count].lines[j],buffer);
+ //           printf("Written at %d:%d\nData:%s\n\n",count,j,smapsb[count].lines[j]);
+
+            b_i=0;
+            j++;
+        }
+        else{
+        buffer[b_i] = string_buffer[i];
+         b_i++;
+        }
+        
+        i++;
+        }
+        buffer[b_i]='\0';
+        strcpy(smapsb[count].lines[j],buffer);
+        
+        
+        
+         
+        
+            
+        
+        //comparison here...
+#ifdef __SMAPTRACK_DEBUG__
+        
+         
 		for(i=0;i<count;i++)
-		{
-			for(j=0;j<count;j++)
+		{ 
+        printf("printing %d...\n",i);
+			for(j=0;j<=smap_elem_line_count;j++)
 			{
 				printf("%s\n",smapsb[i].lines[j]);
 			}
-
+        printf("-----\n");
 		}
+        printf("\n\n----------------------\n\n");
+#else
+        for(i=0;i<count;i++)
+        {
+            for(j=0;j<smap_elem_line_count;j++)
+            {
+                if(strcmp(smapsb[i].lines[j],smapsbprev[i].lines[j])){
+                    
+                    for(j=0;j<smap_elem_line_count;j++){
+                        printf("%s   --->   %s\n",smapsb[i].lines[j],smapsbprev[i].lines[j]);
+                        strcpy(smapsbprev[i].lines[j], smapsb[i].lines[j]);
+                    }
+                    break;
+                    printf("\n\n");
 
-
+                }
+            }
+        }
+#endif
 		sleep(sleep_duration);
 	}
 
