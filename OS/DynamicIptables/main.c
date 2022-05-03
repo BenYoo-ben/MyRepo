ab7e242912fd3915781efa64a0dc15565287338f
BenYoo-ben <neverknown0909@gmail.com>
2022-05-03 08:34:54 +0100

    Modified src files to follow Google C++ Guide

--- OS/DynamicIptables/main.c
+++ OS/DynamicIptables/main.c
@@ -3,145 +3,138 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/wait.h>
+#include <stdint.h>
 
-//predefined buffersize, if your iptable list is super huge, you might want to make it bigger.
-#define __BUF_SIZE__ 32768
-#define __LINE_SIZE__ 1024
+// predefined buffersize, if your iptable list is super huge, you might want to make it bigger.
+#define kBufSize 32768
+#define kLineSze 1024
 
 int main(int argc, char *argv[]) {
-	//predefined sleep duration, adjust it using 1st argument
-	int sleep_sec = 3;
-	//predefined location of iptables, pass 2nd argument if iptables is not located in default directory.
-	char *iptables_loc = "/sbin/iptables";
+    // predefined sleep duration, adjust it using 1st argument
+    int sleep_sec = 3;
+    // predefined location of iptables, pass 2nd argument if iptables is not located in default directory.
+    char *iptables_loc = "/sbin/iptables";
 
-	// change arguments..
-	if (argc >= 2) {
-		sleep_sec = atoi(argv[1]);
-		iptables_loc = argv[2];
-	} else {
-		printf(
-				"using iptables in /sbin/iptables...\nspecify other location if not working...\n%s [sleep time/sec] [iptables location, ex \"/sbin/iptables\"]\n\n",
-				argv[0]);
-	}
+    // change arguments..
+    if (argc >= 2) {
+        sleep_sec = atoi(argv[1]);
+        iptables_loc = argv[2];
+    } else {
+        printf("using iptables in /sbin/iptables...\n"
+                "specify other location if not working...\n"
+                "%s [sleep time/sec] [iptables location, ex \"/sbin/iptables\"]\n\n",
+                argv[0]);
+    }
 
-	//defining buffers...
-	char buffer[__BUF_SIZE__ ];
-	char prev_buffer[__BUF_SIZE__ ];
+    // defining buffers...
+    char buffer[kBufSize ];
+    char prev_buffer[kBufSize ];
 
-	//clearing dummy data with 0.
+    // clearing dummy data with 0.
 
-	memset(buffer,0x0,__BUF_SIZE__);
-	memset(prev_buffer,0x0,__BUF_SIZE__);
-
-
-	//len variables, and flag
-	short prev_len = 0;
-	short len = 0;
-	//flag is more like count in this code.
-	int flag = 0;
-
-	while (1) {
-		//pipe vars
-		int pipe_link[2];
-
-		//pid to be used with fork..
-		pid_t pid;
-
-		//pipe check.
-		if (pipe(pipe_link) == -1) {
-			perror("pipe failed.\n");
-			exit(1);
-		}
-
-		//fork check.
-		if ((pid = fork()) == -1) {
-			perror("fork failed");
-			exit(2);
-		}
-
-		if (pid == 0) {
-			//child process
-
-			//copy STDOUT --> pipe_link[1]
-			dup2(pipe_link[1], STDOUT_FILENO);
-			close(pipe_link[0]);
-			close(pipe_link[1]);
-
-			execl(iptables_loc, "iptables", "-L", "-nv", (char*) 0);
-
-			exit(0);
-		} else {
-
-			//parent
-
-			//close pipe.
-			close(pipe_link[1]);
-
-			//read from redirected stdout --> pipe[0].
-
-			len = read(pipe_link[0], buffer, sizeof(buffer));
-
-			close(pipe_link[0]);
-
-			char line[__LINE_SIZE__ ];
-
-			short longer_line = -1;
-
-			//? statement, comparing now to prev buffer to see which buffer is longer.
-			(len > prev_len) ? (longer_line = len) : (longer_line = prev_len);
-
-			printf(
-					"\n\n%d th check... Displaying changes in %d sec...   read : %d\n",
-					flag, sleep_sec, len);
-
-			if (flag) {
-
-				int i;
-				for (i = 0; i < longer_line; i++) {
-
-					if (buffer[i] != prev_buffer[i]) {
-						int now = i;
-						int start, end;
-						while (i >= 0 && buffer[i] != '\n') {
-							i--;
-						}
-
-						i++;
-						start = i;
-						i = now;
-
-						while (buffer[i] < longer_line && buffer[i] != '\n') {
-							i++;
-						}
-
-						end = i;
-						memset(line, 0x0, __LINE_SIZE__);
-						memcpy(line, buffer + start, end - start + 1);
-
-						line[end - start + 1] = '\0';
-
-						printf("%s\n", line);
-
-					}
-
-				}
-
-			}
+    memset(buffer, 0x00, sizeof(buffer));
+    memset(prev_buffer, 0x00, sizeof(prev_buffer));
 
 
-			//clear and copy data.
-			memset(prev_buffer, 0x0, prev_len);
+    // len variables, and flag
+    int16_t prev_len = 0;
+    int16_t len = 0;
+    // flag is more like count in this code.
+    int flag = 0;
 
-			memcpy(prev_buffer, buffer, len);
+    while (1) {
+        // pipe vars
+        int pipe_link[2];
 
-			memset(buffer, 0x0, len);
-			prev_len = len;
-			flag++;
+        // pid to be used with fork..
+        pid_t pid;
 
-			wait(NULL);
-			sleep(sleep_sec);
-		}
+        // pipe check.
+        if (pipe(pipe_link) == -1) {
+            perror("pipe failed.\n");
+            exit(1);
+        }
 
-	}
-	return 0;
+        // fork check.
+        if ((pid = fork()) == -1) {
+            perror("fork failed");
+            exit(2);
+        }
+
+        if (pid == 0) {
+            // child process
+
+            // copy STDOUT --> pipe_link[1]
+            dup2(pipe_link[1], STDOUT_FILENO);
+            close(pipe_link[0]);
+            close(pipe_link[1]);
+
+            execl(iptables_loc, "iptables", "-L", "-nv", (char *)0);
+
+            exit(0);
+        } else {
+            // parent
+
+            // close pipe.
+            close(pipe_link[1]);
+
+            // read from redirected stdout --> pipe[0].
+
+            len = read(pipe_link[0], buffer, sizeof(buffer));
+
+            close(pipe_link[0]);
+
+            char line[kLineSze ];
+
+            int16_t longer_line = -1;
+
+            // ? statement, comparing now to prev buffer to see which buffer is longer.
+            (len > prev_len) ? (longer_line = len) : (longer_line = prev_len);
+
+            printf("\n\n%d th check... Displaying changes in %d sec...   read : %d\n",
+                    flag, sleep_sec, len);
+
+            if (flag) {
+                int i;
+                for (i = 0; i < longer_line; i++) {
+                    if (buffer[i] != prev_buffer[i]) {
+                        int now = i;
+                        int start, end;
+                        while (i >= 0 && buffer[i] != '\n') {
+                            i--;
+                        }
+
+                        i++;
+                        start = i;
+                        i = now;
+
+                        while (buffer[i] < longer_line && buffer[i] != '\n') {
+                            i++;
+                        }
+
+                        end = i;
+                        memset(line, 0x0, kLineSze);
+                        memcpy(line, buffer + start, end - start + 1);
+
+                        line[end - start + 1] = '\0';
+
+                        printf("%s\n", line);
+                    }
+                }
+            }
+
+            // clear and copy data.
+            memset(prev_buffer, 0x0, prev_len);
+
+            memcpy(prev_buffer, buffer, len);
+
+            memset(buffer, 0x0, len);
+            prev_len = len;
+            flag++;
+
+            wait(NULL);
+            sleep(sleep_sec);
+        }
+    }
+    return 0;
 }
--- OS/smaptrack/main.c
+++ OS/smaptrack/main.c
@@ -1,65 +1,60 @@
-#include <stdio.h> //sprintf, printf, etc
-#include <fcntl.h> //open
-#include <stdlib.h> //exit, atoi
-#include <unistd.h> //lseek, read, sleep
-#include <string.h> //strcpy, strcmp
-#include <time.h> //time measure
+#include <stdio.h>  // sprintf, printf, etc
+#include <fcntl.h>  // open
+#include <stdlib.h>  // exit, atoi
+#include <unistd.h>  // lseek, read, sleep
+#include <string.h>  // strcpy, strcmp
+#include <time.h>  // time measure
 
 /*
-* just some big enough numbers.
-* if using this program in a super limited environment,
-* you might want to adjust values.
-*/
+ * just some big enough numbers.
+ * if using this program in a super limited environment,
+ * you might want to adjust values.
+ */
 #define __SMAPTRACK_DIR_BUFFER_SIZE__ 1024
 #define __SMAPTRACK_BLOCK_LINE_MAXIMUM__ 50
 #define __SMAPTRACK_LINE_LENGTH__ 128
 
 // print every each line, configured.
-//#define __SMAPTRACK_DEBUG__ 
+// #define __SMAPTRACK_DEBUG__
 
-//structure for parsing,
+// structure for parsing,
 struct smaptrack_block {
-	char lines[__SMAPTRACK_BLOCK_LINE_MAXIMUM__][__SMAPTRACK_LINE_LENGTH__];
+    char lines[__SMAPTRACK_BLOCK_LINE_MAXIMUM__][__SMAPTRACK_LINE_LENGTH__];
 };
 
-//clear bits to 0.
-void InitSmaptrackBlock(struct smaptrack_block *sb){
-
-        memset(sb->lines,0x0, __SMAPTRACK_BLOCK_LINE_MAXIMUM__ * __SMAPTRACK_LINE_LENGTH__);
+// clear bits to 0.
+void InitSmaptrackBlock(struct smaptrack_block *sb) {
+    memset(sb->lines, 0x00, __SMAPTRACK_BLOCK_LINE_MAXIMUM__ * __SMAPTRACK_LINE_LENGTH__);
 }
 
-//count line by reading "general" last string in smap (VmFlag)
-int EvaluateSmapElemLineCount(int fd){
+// count line by reading "general" last string in smap (VmFlag)
+int EvaluateSmapElemLineCount(int fd) {
     int line = 0;
     char c;
-    while(1){
-        read(fd,&c,1);
+    while (1) {
+        read(fd, &c, 1);
 
-        if(c=='\n')
+        if (c =='\n') {
             line++;
-        else if(c=='V'){
-            read(fd,&c,1);
-            if(c=='m'){
-                 read(fd,&c,1);
-                 if(c=='F'){
-                    read(fd,&c,1);
-                    if(c=='l')
-                    {
-                        read(fd,&c,1);
-                        if(c=='a'){
-                            read(fd,&c,1);
-                            if(c=='g'){
+        } else if (c == 'V') {
+            read(fd, &c, 1);
+            if (c =='m') {
+                read(fd, &c, 1);
+                if (c =='F') {
+                    read(fd, &c, 1);
+                    if (c =='l') {
+                        read(fd, &c, 1);
+                        if (c =='a') {
+                            read(fd, &c, 1);
+                            if (c =='g') {
                                 break;
                             }
                         }
                     }
-                 }
+                }
             }
-        
         }
-        
     }
-
     return line+1;
 }
 
@@ -72,217 +67,186 @@ int main(int argc, char *argv[]) {
     char grep_block[__SMAPTRACK_BLOCK_LINE_MAXIMUM__][64];
     FILE* smaptrack_file = stdout;
     if (argc < 2) {
-    
-		printf("Usage : %s [# pid] [# duration(sec)]  [--grep=string (option)]\n [--data_count=# (option)] [--file=string (option)] \n", argv[0]);
-		exit(0);
-    }else if(argc > 3){
-        //parse arguments...
+        printf("Usage : %s [# pid] [# duration(sec)]  [--grep=string (option)]\n [--data_count=# (option)] [--file=string (option)] \n", argv[0]);
+        exit(0);
+    } else if (argc > 3) {
+        // parse arguments...
         int iter = 2;
 
-        while(iter < argc-1)
-        {
+        while (iter < argc-1) {
             iter++;
 
             char *c_p = NULL;
-            
-            if( (c_p = strstr(argv[iter],"--grep"))!= NULL){
-                c_p = strstr(c_p,"=");
-                sprintf(grep_block[grep_block_count],"%s",c_p+1);
+
+            if ((c_p = strstr(argv[iter], "--grep")) != NULL) {
+                c_p = strstr(c_p, "=");
+                snprintf(grep_block[grep_block_count], sizeof(grep_block[grep_block_count]), "%s", c_p+1);
                 grep_block[grep_block_count][strlen(grep_block[grep_block_count])]='\0';
                 grep_block_count++;
                 __SMAPTRACK_GREP_TRACK__ = 1;
-            }
-            else if( (c_p = strstr(argv[iter],"--data_count"))!=NULL){
-                c_p = strstr(c_p,"=");
+            } else if ((c_p = strstr(argv[iter], "--data_count")) != NULL) {
+                c_p = strstr(c_p, "=");
                 char buffer[100];
-                sprintf(buffer,"%s",c_p+1);
+                snprintf(buffer, sizeof(buffer), "%s", c_p+1);
                 buffer[strlen(buffer)] = '\0';
                 smaptrack_data_count = atoi(buffer);
-           }else if( (c_p = strstr(argv[iter], "--file"))!=NULL){
-		        c_p = strstr(c_p,"=");
-		        char buffer[100];
-        		sprintf(buffer,"%s",c_p+1);
-	        	buffer[strlen(buffer)] = '\0';
-	        	smaptrack_file = fopen(buffer,"w+");	
-		    
+            } else if ((c_p = strstr(argv[iter], "--file")) != NULL) {
+                c_p = strstr(c_p, "=");
+                char buffer[100];
+                snprintf(buffer, sizeof(buffer), "%s", c_p+1);
+                buffer[strlen(buffer)] = '\0';
+                smaptrack_file = fopen(buffer, "w+");
             }
-
-            
         }
-    
     }
-//initialize variables...
- 
+    // initialize variables...
+
     int smap_elem_line_count = -1;
     int sleep_duration = atoi(argv[2]);
-    
-    long trial_count = 1;
-    char dirloc[__SMAPTRACK_DIR_BUFFER_SIZE__ ];
+
+    unsigned int trial_count = 1;
+    char dirloc[__SMAPTRACK_DIR_BUFFER_SIZE__];
 
     struct smaptrack_block smapsb[smaptrack_data_count];
     struct smaptrack_block smapsbprev[smaptrack_data_count];
-   
-    int i,k;
 
-//more initializations, clear bits...
-    for(i=0;i<smaptrack_data_count;i++){
+    int i, k;
+
+    // more initializations, clear bits...
+
+    for (i = 0; i < smaptrack_data_count; i++) {
         InitSmaptrackBlock(&(smapsb[i]));
         InitSmaptrackBlock(&(smapsbprev[i]));
     }
-	memset(dirloc, 0x0, __SMAPTRACK_DIR_BUFFER_SIZE__);
+    memset(dirloc, 0x00, sizeof(dirloc));
 
-	sprintf(dirloc, "/proc/%d/smaps", atoi(argv[1]));
+    snprintf(dirloc, sizeof(dirloc),  "/proc/%d/smaps", atoi(argv[1]));
 
-    printf("Start logging for %s -- line C:%d -- sleep duration --%d\n\n",dirloc,smap_elem_line_count,sleep_duration);
-    
-	int fd = open(dirloc, O_RDONLY);
+    printf("Start logging for %s -- line C:%d -- sleep duration --%d\n\n", dirloc, smap_elem_line_count, sleep_duration);
 
+    int fd = open(dirloc, O_RDONLY);
 
-    //calculate smap_elem_line_count
-    smap_elem_line_count = EvaluateSmapElemLineCount(fd);   
-    lseek(fd,0,SEEK_SET);
+    // calculate smap_elem_line_count
+    smap_elem_line_count = EvaluateSmapElemLineCount(fd);
+    lseek(fd, 0, SEEK_SET);
+
 #ifdef __SMAPTRACK_DEBUG__
-    fprintf(smaptrack_file,"ELEM COUNT = %d\n",smap_elem_line_count); 
-#endif	
-   
-   //string buffer..
+    fprintf(smaptrack_file, "ELEM COUNT = %d\n", smap_elem_line_count);
+#endif
+
+    // string buffer..
     int chunk_size = smap_elem_line_count * __SMAPTRACK_LINE_LENGTH__ * smaptrack_data_count;
-    
+
     char string_buffer[chunk_size];
 
-    //MAIN LOOP
+    // MAIN LOOP
     while (1) {
-   
-        //time measure, to string format..
+        // time measure, to string format..
         time_t curr_time = time(NULL);
         char * time_str = ctime(&curr_time);
         time_str[strlen(time_str)-1] = '\0';
-        fprintf(smaptrack_file,"\n[%ld] -- %s\n",trial_count++,time_str);
+        fprintf(smaptrack_file, "\n[%ld] -- %s\n", trial_count++, time_str);
 
-		int count = 0;
-		int i=0,j=0;
-        lseek(fd,0,SEEK_SET);
-        
-        //read
-        int read_size=0;
+        int count = 0;
+        int i = 0, j = 0;
+        lseek(fd, 0, SEEK_SET);
+
+        // read
+        int read_size = 0;
         int curr_read = -1;
-        while( (curr_read = read(fd,string_buffer+read_size,chunk_size))>0)
-        {
+        while ((curr_read = read(fd, string_buffer + read_size, chunk_size)) > 0) {
             read_size += curr_read;
         }
 
+        // string parse + prepare struct
+        char buffer[__SMAPTRACK_LINE_LENGTH__];
+        int b_i = 0;
+        while (i < read_size) {
+            if (string_buffer[i] == '\n') {
+                if (j == smap_elem_line_count) {
+                    j = 0;
+                    count++;
+                }
 
-      //string parse + prepare struct
-       char buffer[__SMAPTRACK_LINE_LENGTH__];
-       int b_i=0;
-        while(i<read_size){
-        
-        if(string_buffer[i]=='\n'){
-        
-            if(j==smap_elem_line_count){
-                j=0;
-            count++;
-            
+                buffer[b_i] = '\0';
+                strncpy(smapsb[count].lines[j], buffer, sizeof(smapsb[count].lines[j]));
+
+                b_i = 0;
+                j++;
+            } else {
+                buffer[b_i] = string_buffer[i];
+                b_i++;
             }
-
-            buffer[b_i] = '\0';
-            strcpy(smapsb[count].lines[j],buffer);
-
-            b_i=0;
-            j++;
-        }
-        else{
-        buffer[b_i] = string_buffer[i];
-         b_i++;
-        }
-        
-        i++;
+            i++;
         }
         buffer[b_i]='\0';
-        strcpy(smapsb[count].lines[j],buffer);
-        
-        //comparison here...
+        strncpy(smapsb[count].lines[j], buffer, sizeof(smapsb[count].lines[j]));
+
+        // comparison here...
 #ifdef __SMAPTRACK_DEBUG__
-         
-		for(i=0;i<count;i++)
-		{ 
-        printf("printing %d...\n",i);
-			for(j=0;j<=smap_elem_line_count;j++)
-			{
-				printf("%s\n",smapsb[i].lines[j]);
-			}
-        printf("-----\n");
-		}
+
+        for (i = 0; i < count; i++) {
+            printf("printing %d...\n", i);
+            for (j = 0; j <= smap_elem_line_count; j++) {
+                printf("%s\n", smapsb[i].lines[j]);
+            }
+            printf("-----\n");
+        }
         printf("\n\n----------------------\n\n");
 #else
-if(__SMAPTRACK_GREP_TRACK__ == 1){
-        
-        for(i=0;i<count;i++)
-        {
-            for(j=0;j<smap_elem_line_count;j++)
-            {
-             	//if line != previously read line.
-		   if(strcmp(smapsb[i].lines[j],smapsbprev[i].lines[j])){
-                    
-                     int match = 0;
-                     for(k=0;k<grep_block_count;k++){
-                        // + if line is what you want to grep.
-                        if(strstr(smapsb[i].lines[0],grep_block[k])!= NULL ){
-                            match=1;
-                            break;
-                        }
-                     }
-                    if(match==1){
-			// this is what I wanted to grep, so print it.
-                     for(j=0;j<smap_elem_line_count;j++){
-                            
-                         if(j==0)
-                             fprintf(smaptrack_file,"%s\n",smapsb[i].lines[j]);
-                         else{
-                         fprintf(smaptrack_file,"%-30s%-10s%30s\n",smapsbprev[i].lines[j],"   --->",smapsb[i].lines[j]);
-                         }
-                         strcpy(smapsbprev[i].lines[j], smapsb[i].lines[j]);
-                         
-                    }
-                    
-                    fprintf(smaptrack_file,"\n--------------------------------------------------\n");
-                    }
-                    fflush(smaptrack_file);
-                    break;
-
-                }
-            }
-        }
-}
-else
-{
-        //printf("Running in 'all' mode or none 'grep-ping' mode\n");
-        for(i=0;i<count;i++)
-        {
-            for(j=0;j<smap_elem_line_count;j++)
-            {
-		//hmm... let's see if value's changed compared to previous read event.
-                if(strcmp(smapsb[i].lines[j],smapsbprev[i].lines[j])){
-                    for(j=0;j<smap_elem_line_count;j++){
-                           //print! print!  
-                            if(j==0)
-                                fprintf(smaptrack_file,"%s\n",smapsb[i].lines[j]);
-                            else{
-                            fprintf(smaptrack_file,"%-30s%-10s%30s\n",smapsbprev[i].lines[j],"   --->",smapsb[i].lines[j]);
+        if (__SMAPTRACK_GREP_TRACK__ == 1) {
+            for (i = 0; i < count; i++) {
+                for (j = 0; j < smap_elem_line_count; j++) {
+                    // if line != previously read line.
+                    if (strcmp(smapsb[i].lines[j], smapsbprev[i].lines[j])) {
+                        int match = 0;
+                        for (k = 0; k < grep_block_count; k++) {
+                            // + if line is what you want to grep.
+                            if (strstr(smapsb[i].lines[0], grep_block[k]) != NULL) {
+                                match = 1;
+                                break;
                             }
-                            strcpy(smapsbprev[i].lines[j], smapsb[i].lines[j]);
-                          
+                        }
+                        if (match == 1) {
+                            // this is what I wanted to grep, so print it.
+                            for (j = 0; j < smap_elem_line_count; j++) {
+                                if (j == 0) {
+                                    fprintf(smaptrack_file, "%s\n", smapsb[i].lines[j]);
+                                } else {
+                                    fprintf(smaptrack_file, "%-30s%-10s%30s\n", smapsbprev[i].lines[j], "   --->", smapsb[i].lines[j]);
+                                }
+                                strncpy(smapsbprev[i].lines[j], smapsb[i].lines[j], sizeof(smapsbprev[i].lines[j]));
+                            }
+                            fprintf(smaptrack_file, "\n--------------------------------------------------\n");
+                        }
+                        fflush(smaptrack_file);
+                        break;
                     }
-                    fprintf(smaptrack_file,"\n----------------------------------------------------------\n");
-                    fflush(smaptrack_file);
-                    break;
+                }
+            }
+        } else {
+            // printf("Running in 'all' mode or none 'grep-ping' mode\n");
+            for (i = 0; i < count; i++) {
+                for (j = 0; j < smap_elem_line_count; j++) {
+                    if (strcmp(smapsb[i].lines[j], smapsbprev[i].lines[j])) {
+                        for (j = 0; j < smap_elem_line_count; j++) {
+                            // print!
+                            if (j == 0) {
+                                fprintf(smaptrack_file, "%s\n", smapsb[i].lines[j]);
+                            } else {
+                                fprintf(smaptrack_file, "%-30s%-10s%30s\n", smapsbprev[i].lines[j], "   --->", smapsb[i].lines[j]);
+                            }
+                            strncpy(smapsbprev[i].lines[j], smapsb[i].lines[j], sizeof(smapsbprev[i].lines[j]));
+                        }
+                        fprintf(smaptrack_file, "\n----------------------------------------------------------\n");
+                        fflush(smaptrack_file);
+                        break;
+                    }
                 }
             }
         }
-}
 #endif
-		//wait for a while... could be improved using nanosleep.
-		sleep(sleep_duration);
-	}
-
+        // wait for a while... could be improved using nanosleep.
+        sleep(sleep_duration);
+    }
 }
