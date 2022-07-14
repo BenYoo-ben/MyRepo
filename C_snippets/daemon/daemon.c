a678fa502d0b7a73e1475232b8d70696232772f9
Byongjin Yoo <57353430+BenYoo-ben@users.noreply.github.com>
2022-07-14 10:57:43 +0900

    modify: daemon.c

--- C_snippets/daemon/daemon.c
+++ C_snippets/daemon/daemon.c
@@ -12,44 +12,52 @@ int main(int argc, char *argv[]) {
     // remove control terminal
     // Make it be session leader without control terminal
     // call fork to detach the terminal
-    if (pid == 0) {
-        _exit(3);
-    } else if (pid > 0) {
+    if (pid > 0) {
+        _exit(0);
+    } else if (pid == 0) {
         // I'm session leader without terminal now
-        setsid();
+        if (setsid() == -1) {
+            perror("setsid failure");
+        }
 
         // session leader may get terminal again later.
         // call fork to prevent it from happening
         // which will make this process non-session leader
 
         int pid2 = fork();
-        if (pid2 == 0) {
-            _exit(4);
-        } else if (pid2 > 0) {
+        if (pid2 > 0) {
+            _exit(0);
+        } else if (pid2 == 0) {
+            // do nothing
         } else {
+            perror("fork 2nd failure\n");
             exit(1);
         }
     } else {
-        perror("fork went south\n");
+        perror("fork failure\n");
         exit(1);
     }
 
-    // close all stdI/Os and open them on /dev/null/
+    // zone of grandchild
+    // close all stdI/Os and reopen them at /dev/null/ 
     stdin = freopen("/dev/null", "r", stdin);
     stdout = freopen("/dev/null", "w", stdout);
     stderr = freopen("/dev/null", "w", stderr);
 
     umask(0);
 
+    // log pid
     char buf[LOG_FILE_SIZE];
     snprintf(buf, LOG_FILE_SIZE, "/var/run/byd.%u", getpid());
 
     int fd = open(buf, O_CREAT | O_TRUNC | O_WRONLY);
 
     if (fd < 0) {
-        printf("open went south \n");
+        printf("open log.pid failure \n");
         exit(7);
     }
     write(fd, buf, LOG_FILE_SIZE);
     close(fd);
+    
+    // do demon's job
 }
